99679e97063029ddb03634121b13daecce110a54
juserh <rosapark07@gmail.com>
2024-04-02 23:57:33 +0900

    프로젝트 생성

---
+++ src/test/java/com/mungtamjung/petective/PetectiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mungtamjung.petective;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetectiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
